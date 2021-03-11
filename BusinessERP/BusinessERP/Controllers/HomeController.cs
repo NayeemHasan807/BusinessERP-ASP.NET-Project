473cc96b5083ee2e75f414c9debf7015853cdf2c
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-11 23:42:59 +0600

    HomeController and other public view pages with necessary content

---
+++ BusinessERP/BusinessERP/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusinessERP.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
