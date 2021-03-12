a47f87f3478312c63ba08c768dcde45316251455
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-12 22:55:35 +0600

    Change password module with validation for admin

--- BusinessERP/BusinessERP/Controllers/AdminController.cs
+++ BusinessERP/BusinessERP/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using BusinessERP.Repositories;
+﻿using BusinessERP.Models.ViewModels;
+using BusinessERP.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace BusinessERP.Controllers
     {
         private EmployeeRepository employeerepo = new EmployeeRepository();
         private JobCategoryRepository jobcatrepo = new JobCategoryRepository();
+        private UserRepository userrepo = new UserRepository();
         public bool CheckAccess()
         {
             if (Session["UserType"].ToString() == "Admin")
@@ -41,6 +43,41 @@ namespace BusinessERP.Controllers
             }
             else
                 return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChangePassword(ChangePasswordViewModel change)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = userrepo.GetByUserName(Session["UserName"].ToString());
+                if(change.Password==user.Password)
+                {
+                    if(change.NewPassword == change.ReNewPassword)
+                    {
+                        user.Password = change.NewPassword;
+                        userrepo.Update(user);
+                        return RedirectToAction("MyProfile","Admin");
+                    }
+                    else
+                    {
+                        TempData["NpAndRnp"] = "New password and retype new password must need to be same";
+                        return View();
+
+                    }
+                }
+                else
+                {
+                    TempData["OldP"] = "Old password is incorrect";
+                    return View();
+                }
+            }
+            else
+                return View();
         }
     }
 }
---
+++ BusinessERP/BusinessERP/Models/MetaDataModels/UserMetaData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessERP.Models.MetaDataModels
+{
+    public class UserMetaData
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+        public string UserStatus { get; set; }
+    }
+}
