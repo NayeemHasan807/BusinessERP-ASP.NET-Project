3184e730f56ecd4431bd0f618e2b57db4cdd16ed
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-12 18:03:26 +0600

    Employee Repository

---
+++ BusinessERP/BusinessERP/Repositories/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using BusinessERP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessERP.Repositories
+{
+    public class EmployeeRepository:Repository<Employee>
+    {
+        public Employee GetByUserName(string username)
+        {
+            return context.Employees.Where(x => x.UserName == username).FirstOrDefault();
+        }
+    }
+}
