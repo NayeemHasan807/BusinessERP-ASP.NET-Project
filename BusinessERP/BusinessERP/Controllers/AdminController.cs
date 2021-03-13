9a15674f90de020e51f0e73fc5c35e4539863d04
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-14 04:07:02 +0600

    Employee management module for admin

--- BusinessERP/BusinessERP/Controllers/AdminController.cs
+++ BusinessERP/BusinessERP/Controllers/AdminController.cs
@@ -15,13 +15,16 @@ namespace BusinessERP.Controllers
         private EmployeeRepository employeerepo = new EmployeeRepository();
         private JobCategoryRepository jobcatrepo = new JobCategoryRepository();
         private UserRepository userrepo = new UserRepository();
+        private NoticeRepository noticerepo = new NoticeRepository();
+        //Check access to admin actions
         public bool CheckAccess()
         {
-            if (Session["UserType"].ToString() == "Admin")
+            if (HttpContext.Session["UserType"].ToString() == "Admin")
                 return true;
             else
                 return false;
         }
+        //admin dashboard
         [HttpGet]
         public ActionResult Index()
         {
@@ -33,6 +36,54 @@ namespace BusinessERP.Controllers
             else
                 return RedirectToAction("Login", "Home");
         }
+        
+        //Admin actions on his/her access password
+        [HttpGet]
+        public ActionResult ChangePassword()
+        {
+            if (CheckAccess())
+            {
+                return View();
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpPost]
+        public ActionResult ChangePassword(ChangePasswordViewModel change)
+        {
+            if (CheckAccess())
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = userrepo.GetByUserName(Session["UserName"].ToString());
+                    if(change.Password==user.Password)
+                    {
+                        if(change.NewPassword == change.ReNewPassword)
+                        {
+                            user.Password = change.NewPassword;
+                            userrepo.Update(user);
+                            return RedirectToAction("MyProfile","Admin");
+                        }
+                        else
+                        {
+                            TempData["NpAndRnp"] = "New password and retype new password must need to be same";
+                            return View();
+
+                        }
+                    }
+                    else
+                    {
+                        TempData["OldP"] = "Old password is incorrect";
+                        return View();
+                    }
+                }
+                else
+                    return View();
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        //Admin actions on his/her profile
         [HttpGet]
         public ActionResult MyProfile()
         {
@@ -47,98 +98,162 @@ namespace BusinessERP.Controllers
                 return RedirectToAction("Login", "Home");
         }
         [HttpGet]
-        public ActionResult ChangePassword()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult ChangePassword(ChangePasswordViewModel change)
-        {
-            if (ModelState.IsValid)
-            {
-                var user = userrepo.GetByUserName(Session["UserName"].ToString());
-                if(change.Password==user.Password)
-                {
-                    if(change.NewPassword == change.ReNewPassword)
-                    {
-                        user.Password = change.NewPassword;
-                        userrepo.Update(user);
-                        return RedirectToAction("MyProfile","Admin");
-                    }
-                    else
-                    {
-                        TempData["NpAndRnp"] = "New password and retype new password must need to be same";
-                        return View();
-
-                    }
-                }
-                else
-                {
-                    TempData["OldP"] = "Old password is incorrect";
-                    return View();
-                }
-            }
-            else
-                return View();
-        }
-        [HttpGet]
         public ActionResult UpdateProfile()
         {
-            var profile = employeerepo.GetByUserName(Session["UserName"].ToString());
-            TempData["JobCategory"] = jobcatrepo.GetAll();
-            return View(profile);
+            if (CheckAccess())
+            {
+                var profile = employeerepo.GetByUserName(Session["UserName"].ToString());
+                TempData["JobCategory"] = jobcatrepo.GetAll();
+                return View(profile);
+            }
+            else
+                return RedirectToAction("Login", "Home");
+
         }
         [HttpPost]
         public ActionResult UpdateProfile(Employee employee, HttpPostedFileBase image)
         {
-            var profile = employeerepo.GetByUserName(Session["UserName"].ToString());
-            TempData["JobCategory"] = jobcatrepo.GetAll();
-            if (ModelState.IsValid)
+            if (CheckAccess())
             {
-                if(image != null)
+                var profile = employeerepo.GetByUserName(Session["UserName"].ToString());
+                TempData["JobCategory"] = jobcatrepo.GetAll();
+                if (ModelState.IsValid)
                 {
-                    if (Path.GetExtension(image.FileName)==".jpg" | Path.GetExtension(image.FileName) == ".png")
+                    if(image != null)
                     {
-                        string name = Path.GetFileNameWithoutExtension(image.FileName);
-                        string extention = Path.GetExtension(image.FileName);
-                        name = name + DateTime.Now.ToString("yyyy-MM-dd")+extention;
-                        string ProfilePicture = "~/Content/ProfilePictures/" + name;
-                        string filepath = Path.Combine(Server.MapPath("~/Content/ProfilePictures/"),name);
-                        image.SaveAs(filepath);
+                        if (Path.GetExtension(image.FileName)==".jpg" | Path.GetExtension(image.FileName) == ".png")
+                        {
+                            string name = Path.GetFileNameWithoutExtension(image.FileName);
+                            string extention = Path.GetExtension(image.FileName);
+                            name = name + DateTime.Now.ToString("yyyy-MM-dd")+extention;
+                            string ProfilePicture = "~/Content/ProfilePictures/" + name;
+                            string filepath = Path.Combine(Server.MapPath("~/Content/ProfilePictures/"),name);
+                            image.SaveAs(filepath);
 
+                            profile.EmployeeName = employee.EmployeeName;
+                            profile.Email = employee.Email;
+                            profile.Gender = employee.Gender;
+                            profile.DateOfBirth = employee.DateOfBirth;
+                            profile.Address = employee.Address;
+                            profile.JoiningDate = employee.JoiningDate;
+                            profile.ProfilePicture = ProfilePicture;
+
+                            employeerepo.Update(profile);
+                            return RedirectToAction("MyProfile", "Admin");
+                        }
+                        else
+                        {
+                            TempData["Error"] = "Profile picture must need to be type '.jpg' or '.png'";
+                            return View(profile);
+                        }
+                    }
+                    else
+                    {
                         profile.EmployeeName = employee.EmployeeName;
                         profile.Email = employee.Email;
                         profile.Gender = employee.Gender;
                         profile.DateOfBirth = employee.DateOfBirth;
                         profile.Address = employee.Address;
                         profile.JoiningDate = employee.JoiningDate;
-                        profile.JobId = employee.JobId;
-                        profile.ProfilePicture = ProfilePicture;
 
                         employeerepo.Update(profile);
-                        return RedirectToAction("MyProfile", "Admin");
+                        return RedirectToAction("MyProfile","Admin");
                     }
-                    else
-                    {
-                        TempData["Error"] = "Profile picture must need to be type '.jpg' or '.png'";
-                        return View(profile);
-                    }
+                } 
+                return View(profile);
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        //Admin actions on notice
+        [HttpGet]
+        public ActionResult CreateNewNotice()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateNewNotice(Notice notice)
+        {
+            if (CheckAccess())
+            {
+                if (ModelState.IsValid)
+                {
+                    noticerepo.Insert(notice);
+                    return RedirectToAction("ViewAllNotice","Admin");
                 }
                 else
                 {
-                    profile.EmployeeName = employee.EmployeeName;
-                    profile.Email = employee.Email;
-                    profile.Gender = employee.Gender;
-                    profile.DateOfBirth = employee.DateOfBirth;
-                    profile.Address = employee.Address;
-                    profile.JoiningDate = employee.JoiningDate;
-                    profile.JobId = employee.JobId;
-
-                    employeerepo.Update(profile);
-                    return RedirectToAction("MyProfile","Admin");
+                    TempData["NoticeTitle"] = notice.NoticeTitle;
+                    TempData["NoticeBody"] = notice.NoticeBody;
+                    return View();
                 }
-            } 
-            return View(profile);
+            }
+            else
+                return RedirectToAction("Login", "Home");
+
         }
+        [HttpGet]
+        public ActionResult ViewAllNotice()
+        {
+            if (CheckAccess())
+            {
+                return View(noticerepo.GetAll());
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public ActionResult NoticeDetails(int id)
+        {
+            if (CheckAccess())
+            {
+                return View(noticerepo.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public ActionResult NoticeDelete(int id)
+        {
+            if (CheckAccess())
+            {
+                return View(noticerepo.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpPost,ActionName("NoticeDelete")]
+        public ActionResult ConfirmNoticeDelete(int id)
+        {
+            if (CheckAccess())
+            {
+                noticerepo.Delete(id);
+                return RedirectToAction("ViewAllNotice", "Admin");
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public ActionResult NoticeEdit(int id)
+        {
+            if (CheckAccess())
+            {
+                return View(noticerepo.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpPost]
+        public ActionResult NoticeEdit(int id,Notice notice)
+        {
+            if (CheckAccess())
+            {
+                noticerepo.Update(notice);
+                return RedirectToAction("ViewAllNotice","Admin");
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+
     }
 }
---
+++ BusinessERP/BusinessERP/Controllers/EmployeeController.cs
@@ -0,0 +1,187 @@
+﻿using BusinessERP.Models;
+using BusinessERP.Repositories;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BusinessERP.Controllers
+{
+    public class EmployeeController : BaseController
+    {
+        private EmployeeRepository employeerepo = new EmployeeRepository();
+        private UserRepository userrepo = new UserRepository();
+        private JobCategoryRepository jobcatrepo = new JobCategoryRepository();
+        public bool CheckAccess()
+        {
+            if (Session["UserType"].ToString() == "Admin")
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            if(CheckAccess())
+            {
+                return View(employeerepo.GetAll());
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (CheckAccess())
+            {
+                TempData["JobCategory"] = jobcatrepo.GetAll();
+                return View();
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee,HttpPostedFileBase image)
+        {
+            if (CheckAccess())
+            {
+                if (ModelState.IsValid)
+                {
+                    var person = userrepo.GetByUserName(employee.UserName);
+                    if(person == null)
+                    {
+                        if (image != null)
+                        {
+                            if (Path.GetExtension(image.FileName) == ".jpg" | Path.GetExtension(image.FileName) == ".png")
+                            {
+                                string name = Path.GetFileNameWithoutExtension(image.FileName);
+                                string extention = Path.GetExtension(image.FileName);
+                                name = name + DateTime.Now.ToString("yyyy-MM-dd") + extention;
+                                string ProfilePicture = "~/Content/ProfilePictures/" + name;
+                                string filepath = Path.Combine(Server.MapPath("~/Content/ProfilePictures/"), name);
+                                image.SaveAs(filepath);
+                                employee.ProfilePicture = ProfilePicture;
+                                User user = new User();
+                                JobCategory job = jobcatrepo.GetById(employee.JobId);
+                                user.UserName = employee.UserName;
+                                user.Password = employee.UserName;
+                                user.UserType = job.JobTitle;
+                                user.UserStatus = employee.Status;
+                                employeerepo.Insert(employee);
+                                userrepo.Insert(user);
+                                return RedirectToAction("Index");
+                            }
+                            else
+                            {
+                                TempData["Error2"] = "Profile picture must need to be type '.jpg' or '.png'";
+                                TempData["JobCategory"] = jobcatrepo.GetAll();
+                                return View(employee);
+                            }
+                        }
+                        TempData["Error2"] = "Must need to add a profile picture";
+                        TempData["JobCategory"] = jobcatrepo.GetAll();
+                        return View(employee);
+                    }
+                    TempData["Error1"] = "This username is taken";
+                    TempData["JobCategory"] = jobcatrepo.GetAll();
+                    return View(employee);
+
+                }
+                TempData["JobCategory"] = jobcatrepo.GetAll();
+                return View(employee);
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            if (CheckAccess())
+            {
+                return View(employeerepo.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            if (CheckAccess())
+            {
+                return View(employeerepo.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult ConfirmDelete(int id, string username)
+        {
+            if (CheckAccess())
+            {
+                employeerepo.Delete(id);
+                userrepo.DeleteByUsername(username);
+                return RedirectToAction("Index", "Employee");
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            if (CheckAccess())
+            {
+                TempData["JobCategory"] = jobcatrepo.GetAll();
+                return View(employeerepo.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Employee employee,HttpPostedFileBase image)
+        {
+            if (CheckAccess())
+            {
+                if (ModelState.IsValid)
+                {
+                    if (image != null)
+                    {
+                        if (Path.GetExtension(image.FileName) == ".jpg" | Path.GetExtension(image.FileName) == ".png")
+                        {
+                            string name = Path.GetFileNameWithoutExtension(image.FileName);
+                            string extention = Path.GetExtension(image.FileName);
+                            name = name + DateTime.Now.ToString("yyyy-MM-dd") + extention;
+                            string ProfilePicture = "~/Content/ProfilePictures/" + name;
+                            string filepath = Path.Combine(Server.MapPath("~/Content/ProfilePictures/"), name);
+                            image.SaveAs(filepath);
+
+                            employee.ProfilePicture = ProfilePicture;
+
+                            employeerepo.Update(employee);
+                            return RedirectToAction("Index", "Employee");
+                        }
+                        else
+                        {
+                            TempData["Error"] = "Profile picture must need to be type '.jpg' or '.png'";
+                            TempData["JobCategory"] = jobcatrepo.GetAll();
+                            return View(employeerepo.GetById(id));
+                        }
+                    }
+                    else
+                    {
+                        employeerepo.Update(employee);
+                        return RedirectToAction("Index", "Employee");
+                    }
+                }
+                TempData["JobCategory"] = jobcatrepo.GetAll();
+                return View(employeerepo.GetById(id));
+            }
+            else
+                return RedirectToAction("Login", "Home");
+        }
+    }
+}
