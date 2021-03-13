6fb10f37ec813221b8f47e9095c8c2eafa5175d3
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-14 00:31:23 +0600

    Admin notice module

---
+++ BusinessERP/BusinessERP/Models/NoticeMetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusinessERP.Models
+{
+    public class NoticeMetaData
+    {
+        public int NoticeId { get; set; }
+        [Required,MinLength(10),MaxLength(30)]
+        public string NoticeTitle { get; set; }
+        [Required,MinLength(20),MaxLength(200)]
+        public string NoticeBody { get; set; }
+        [Required]
+        public string ReceiverType { get; set; }
+    }
+}
