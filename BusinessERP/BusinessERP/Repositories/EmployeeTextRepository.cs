600f88f49f0597140371c8fbf247a3d238af1edc
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-14 17:51:04 +0600

    Employee text module for all type of employee

---
+++ BusinessERP/BusinessERP/Repositories/EmployeeTextRepository.cs
@@ -0,0 +1,20 @@
+﻿using BusinessERP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessERP.Repositories
+{
+    public class EmployeeTextRepository:Repository<EmployeeText>
+    {
+        public List<EmployeeText> GetAllReceivedByUserName(string username)
+        {
+            return context.EmployeeTexts.Where(x=> x.ReceiverUserName == username).ToList();
+        }
+        public List<EmployeeText> GetAllSentByUserName(string username)
+        {
+            return context.EmployeeTexts.Where(x => x.SenderUserName == username).ToList();
+        }
+    }
+}
