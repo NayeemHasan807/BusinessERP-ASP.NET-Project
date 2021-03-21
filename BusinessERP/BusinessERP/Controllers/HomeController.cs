f270fb4a21e1adf99cc21534451aec27b45f202f
NayeemHasan807 <moonnayeem87@gmail.com>
2021-03-21 20:10:14 +0600

    Registration module for customer and vendor

--- BusinessERP/BusinessERP/Controllers/HomeController.cs
+++ BusinessERP/BusinessERP/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BusinessERP.Models;
+using BusinessERP.Repositories;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +11,9 @@ namespace BusinessERP.Controllers
 {
     public class HomeController : Controller
     {
+        private UserRepository userrepo = new UserRepository();
+        private RegistrationRequestRepository rrrepo = new RegistrationRequestRepository();
+
         [HttpGet]
         public ActionResult Index()
         {
@@ -42,6 +48,66 @@ namespace BusinessERP.Controllers
         public ActionResult Registration()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(RegistrationRequest registration, string CPassword, HttpPostedFileBase image)
+        {
+            if(ModelState.IsValid)
+            {
+                if(registration.Password.ToString() == CPassword)
+                {
+                    var person = userrepo.GetByUserName(registration.UserName);
+                    if (person == null)
+                    {
+                        if (image != null)
+                        {
+                            if (Path.GetExtension(image.FileName) == ".jpg" | Path.GetExtension(image.FileName) == ".png")
+                            {
+                                string name = Path.GetFileNameWithoutExtension(image.FileName);
+                                string extention = Path.GetExtension(image.FileName);
+                                name = name + DateTime.Now.ToString("yyyy-MM-dd") + extention;
+                                string ProfilePicture = "~/Content/ProfilePictures/" + name;
+                                string filepath = Path.Combine(Server.MapPath("~/Content/ProfilePictures/"), name);
+                                image.SaveAs(filepath);
+                                registration.ProfilePicture = ProfilePicture;
+                                rrrepo.Insert(registration);
+                                TempData["Confirmation"] = "Your registration request submited successfully. We will verify your information soon and will give you confirmation.";
+                                return RedirectToAction("Index");
+                            }
+                            else
+                            {
+                                TempData["CPassword"] = CPassword;
+                                TempData["Error2"] = "Profile picture must need to be type '.jpg' or '.png'";
+                                return View(registration);
+                            }
+                        }
+                        else
+                        {
+                            TempData["CPassword"] = CPassword;
+                            TempData["Error2"] = "Must need to add a profile picture";
+                            return View(registration);
+                        }
+                    }
+                    else
+                    {
+                        TempData["CPassword"] = CPassword;
+                        TempData["Error1"] = "This username is taken";
+                        return View(registration);
+                    }
+                }
+                else
+                {
+                    TempData["CPassword"] = CPassword;
+                    TempData["PError"] = "Password & Confirm Password Must Need To Be Same!!!";
+                    return View(registration);
+                }
+            }
+            else
+            {
+                TempData["CPassword"] = CPassword;
+                return View(registration);
+            }
         }
     }
 }
---
+++ BusinessERP/BusinessERP/Models/RegistrationRequestMetaData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BusinessERP.Models
+{
+    public class RegistrationRequestMetaData
+    {
+        public int RegistrationId { get; set; }
+        [Required, MinLength(4), MaxLength(30), Display(Name = "Full Name")]
+        public string Name { get; set; }
+        [Required, MinLength(3)]
+        public string UserName { get; set; }
+        [Required,MinLength(8)]
+        public string Password { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public System.DateTime DateOfBirth { get; set; }
+        [Required,MinLength(10)]
+        public string Address { get; set; }
+        public string ProfilePicture { get; set; }
+        [Required]
+        public string UserType { get; set; }
+    }
+}
